a1e443142dea77872acfcf5142902b362468b766
Leandro Alvim <alvim.lgm@live.com>
2017-11-26 20:38:24 -0200

    ok

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,7 @@
 using Perceptrons
 using Base.Test
 
-#include("utils_test.jl")
-#include("linear_perceptron_test.jl")
-#include("kernel_perceptron_test.jl")
+include("utils_test.jl")
+include("linear_perceptron_test.jl")
+include("kernel_perceptron_test.jl")
 include("voted_perceptron_test.jl")
