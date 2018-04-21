a48d942e9c2b94032b93dda889c0f83f55cf4f33
Leandro Alvim <alvim.lgm@live.com>
2018-04-21 00:31:51 -0300

    Averaged perceptron tests included

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,4 @@
+reload("Perceptrons")
 using Perceptrons
 using Base.Test
 
@@ -5,3 +6,4 @@ include("utils_test.jl")
 include("linear_perceptron_test.jl")
 include("kernel_perceptron_test.jl")
 include("voted_perceptron_test.jl")
+include("averaged_perceptron_test.jl")
