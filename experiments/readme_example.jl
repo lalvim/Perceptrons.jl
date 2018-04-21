45759311ac0f6f361eea497e5b4a651905e8404d
Leandro Alvim <alvim.lgm@live.com>
2018-04-21 00:30:36 -0300

    added averaged perceptron example

--- experiments/readme_example.jl
+++ experiments/readme_example.jl
@@ -1,6 +1,6 @@
 using Perceptrons
 
-# training a linear perceptron
+# training a linear perceptron (solving the OR problem)
 X_train        = [1.0 1.0; 0.0 1.0; 1.0 0.0; 0.0 0.0]
 Y_train        = [1; 1; 1; 0.0]
 X_test         = [.8 .9; .01 1; .9 0.2; 0.1 0.2]
@@ -10,14 +10,19 @@ Y_pred         = Perceptrons.predict(model,X_test)
 
 println("[Perceptron] accuracy : $(acc(Y_train,Y_pred))")
 
-# training a voted perceptron
+# training a voted perceptron (solving the OR problem)
 model   = Perceptrons.fit(X_train,Y_train,centralize=true,mode="voted")
 Y_pred  = Perceptrons.predict(model,X_test)
 
 println("[Voted Perceptron] accuracy : $(acc(Y_train,Y_pred))")
 
+# training a averaged perceptron (solving the OR problem)
+model   = Perceptrons.fit(X_train,Y_train,centralize=true,mode="averaged")
+Y_pred  = Perceptrons.predict(model,X_test)
 
-# training a kernel perceptron (XOR)
+println("[Averaged Perceptron] accuracy : $(acc(Y_train,Y_pred))")
+
+# training a kernel perceptron (solving the XOR problem)
 X_train = [1.0 1.0; 0.0 1.0; 1.0 0.0; 0.0 0.0]
 Y_train = [0.0 ; 1.0; 1.0; 0.0]
 X_test  = X_train .+ .03 # adding noise
